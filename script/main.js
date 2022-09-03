2b941a907e59a54f608da43c340a4ecc23f0ec2b
Voicuu <andrei.voicu2@student.upt.ro>
2022-09-03 07:12:50 +0300

    dasda

--- script/main.js
+++ script/main.js
@@ -221,8 +221,8 @@ const animationTimeline = () => {
       "-=2"
     )
     .from(".hat", 0.5, {
-      x: -400,
-      y: 50,
+      x: 150,
+      y: 250,
       rotation: -180,
       opacity: 0
     })
